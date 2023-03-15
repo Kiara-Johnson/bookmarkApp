8737974761c5c08334240df6c17b9d8981300932
Kiara Johnson <johns.kia35@gmail.com>
2023-03-15 15:52:45 -0600

    CSS Style

---
+++ src/components/CreateBookmark/CreateBookmark.js
@@ -0,0 +1,25 @@
+import styles from './CreateBookmark.module.css'
+export default function CreateBookmark ({
+    createBookmark,
+    bookmark,
+    handleChange
+  }) {
+    return (
+      <>
+        <h2>Create A Bookmark</h2>
+        <div className={styles.container}>
+        <form
+        className={styles.form}
+        onSubmit={(e) => {
+          e.preventDefault()
+          createBookmark()
+        }}
+        >
+          <input type='text' value={bookmark.title} name='title' onChange={handleChange} placeholder='Title' />
+          <input type='text' value={bookmark.url} name='url' onChange={handleChange} placeholder='URL' />
+          <input type='submit' value='Create Bookmark' />
+        </form>
+        </div>
+      </>
+    )
+  }
